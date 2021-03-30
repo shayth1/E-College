bc2aeaa1dae6c004e531ca4bfcfc2b4ccee1b8a1
shayth1 <shaythwork@gmail.com>
2021-03-30 22:40:18 +0300

    add reset password

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -1,15 +1,21 @@
 <?php
 // app vars
-if (!defined('APP_NAME')) define('APP_NAME', 'E-College');
-if (!defined('APP_ORGANIZATION')) define('APP_ORGANIZATION', 'WISE');
-if (!defined('APP_OWNER')) define('APP_OWNER', 'Haneen, Dana, Nedal');
-if (!defined('APP_DECRIPTION')) define('APP_DECRIPTION', 'E-College Learning System');
-if (!defined('ALLOWED_INACTIVITY_TIME')) define('ALLOWED_INACTIVITY_TIME', time() + 1 * 60);
+if (!defined('APP_NAME'))                       define('APP_NAME', 'E-College');
+if (!defined('APP_ORGANIZATION'))               define('APP_ORGANIZATION', 'WISE');
+if (!defined('APP_OWNER'))                      define('APP_OWNER', 'Haneen, Dana, Nedal');
+if (!defined('APP_DECRIPTION'))                 define('APP_DECRIPTION', 'E-College Learning System');
+if (!defined('ALLOWED_INACTIVITY_TIME'))            define('ALLOWED_INACTIVITY_TIME', time() + 1 * 60);
 // db vars
-if (!defined('DB_DATABASE')) define('DB_DATABASE', 'ecollege');
-if (!defined('DB_HOST')) define('DB_HOST', '127.0.0.1');
-if (!defined('DB_USERNAME')) define('DB_USERNAME', 'root');
-if (!defined('DB_PASSWORD')) define('DB_PASSWORD', '');
-if (!defined('BD_PORT')) define('BD_PORT', '');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'ecollege');
+if (!defined('DB_HOST'))                        define('DB_HOST', '127.0.0.1');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME', 'root');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD', '');
+if (!defined('BD_PORT'))                        define('BD_PORT', '');
 date_default_timezone_set('asia/amman');
+//mail vars
+if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
+if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'PUT YOUR EMAIL HERE');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'PASSWORD');
+if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'tls');
+if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 587);
 //auto sending email
